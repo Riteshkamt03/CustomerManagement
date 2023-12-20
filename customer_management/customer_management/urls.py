1c9891c8be8d1a77ea7d44b5ea1f92964bdbf6b2
Ritesh Kamat <Riteshkamat03@gmail.com>
2023-12-20 20:43:40 +0530

    changed url to show customer list at home page

--- customer_management/customer_management/urls.py
+++ customer_management/customer_management/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('customers/', include('customers.urls')),
+    path('', include('customers.urls')),
 ]
 
