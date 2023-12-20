6c4c41528e7d8cba9d29c27abe57671d78aceef4
Ritesh Kamat <Riteshkamat03@gmail.com>
2023-12-20 21:23:53 +0530

    Rearranged URLs

--- customer_management/customers/urls.py
+++ customer_management/customers/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from .views import customer_list, customer_detail, customer_new, customer_edit, customer_delete
 
 urlpatterns = [
-    path('', customer_list, name='customer_list'),
-    path('<int:pk>/', customer_detail, name='customer_detail'),
-    path('new/', customer_new, name='customer_new'),
+    path('', customer_new, name='customer_new'),
+    path('cust_list/', customer_list, name='customer_list'),
+    path('<int:pk>/', customer_detail, name='customer_detail'),    
     path('<int:pk>/edit/', customer_edit, name='customer_edit'),
     path('<int:pk>/delete/', customer_delete, name='customer_delete'),
 ]
+
+
+  #  path('', customer_new, name='customer_new'),
+   # path('cust_list/', customer_list, name='customer_list'),
+
